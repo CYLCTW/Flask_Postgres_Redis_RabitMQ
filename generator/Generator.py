fb1bbe965ab8bdbdd5dbc70e79dc9551e2f97834
CYLCTW <70808279+CYLCTW@users.noreply.github.com>
2022-05-24 22:52:22 +0300

    Deleted not working Generator. Generator is in another repository

--- generator/Generator.py
+++
@@ -1,7 +0,0 @@
-import requests
-url = 'http://localhost:5000/sendMessage'
-payload = {'user': 'Al-turar', 'message': 'hello'}
-
-for i in range(10):
-    print("Message send")
-    r = requests.post(url, data=payload)
